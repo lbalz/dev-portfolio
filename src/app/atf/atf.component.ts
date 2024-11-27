55af5c7f8e0ccecd7b4b2626ed789ca359532e16
Lukas Balz <lukasbalz.business@gmail.com>
2024-11-27 18:05:29 +0100

    current changes

--- src/app/atf/atf.component.ts
+++ src/app/atf/atf.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-atf',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './atf.component.html',
   styleUrl: './atf.component.scss'
 })
