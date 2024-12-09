9b5b4979799d0a8ec6d86925fb8413c43f26b44e
Lukas Balz <lukasbalz.business@gmail.com>
2024-12-09 13:30:07 +0100

    about-me first content | removed featured-projects added portfolio-projects

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillSetComponent } from "./skill-set/skill-set.component";
-import { FeaturedProjectsComponent } from "./featured-projects/featured-projects.component";
 import { ReferencesComponent } from "./references/references.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
+import { PortfolioProjectsComponent } from "./portfolio-projects/portfolio-projects.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, SkillSetComponent, FeaturedProjectsComponent, ReferencesComponent, ContactMeComponent],
+  imports: [AboutMeComponent, SkillSetComponent, ReferencesComponent, ContactMeComponent, PortfolioProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
---
+++ src/app/main-content/portfolio-projects/portfolio-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-projects.component.html',
+  styleUrl: './portfolio-projects.component.scss'
+})
+export class PortfolioProjectsComponent {
+
+}
