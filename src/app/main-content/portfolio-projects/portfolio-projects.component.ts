5623e5e74c7d19b89cfc4455a6553283a3525f55
Lukas Balz <lukasbalz.business@gmail.com>
2024-12-15 10:51:11 +0100

    first portfolio projects setup

--- src/app/main-content/portfolio-projects/portfolio-projects.component.ts
+++ src/app/main-content/portfolio-projects/portfolio-projects.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateLanguageService } from '../../translate-language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './portfolio-projects.component.html',
   styleUrl: './portfolio-projects.component.scss'
 })
 export class PortfolioProjectsComponent {
 
+  isEnglish: boolean = true;
+
+  constructor(private languageService: TranslateLanguageService) { }
+
+  translate = inject(TranslateLanguageService);
+
+  ngOnInit(): void {
+    this.languageService.isEnglish$.subscribe(value => {
+      this.isEnglish = value;
+    })
+  }
 }
