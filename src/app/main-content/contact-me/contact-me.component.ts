c9639a24c05557b13ebb15c243415cdbc9dce157
Lukas Balz <lukasbalz.business@gmail.com>
2025-01-07 17:20:26 +0100

    contact me left wrapper

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateLanguageService } from '../../translate-language.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+
+
+
+  // Translation Service
+  isEnglish: boolean = true;
+
+  constructor(private languageService: TranslateLanguageService) { }
+
+  translate = inject(TranslateLanguageService);
+
+  ngOnInit(): void {
+    this.languageService.isEnglish$.subscribe(value => {
+      this.isEnglish = value;
+    })
+  }
 }
