a368bcec434fbf3f23a257bb79dbfe36cc6a53e9
Lukas Balz <lukasbalz.business@gmail.com>
2025-01-07 16:40:01 +0100

    first reference

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateLanguageService } from '../../translate-language.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
 
+
+
+
+  // Translation Service
+  isEnglish: boolean = true;
+
+  constructor(private languageService: TranslateLanguageService) { }
+
+  translate = inject(TranslateLanguageService);
+
+  ngOnInit(): void {
+    this.languageService.isEnglish$.subscribe(value => {
+      this.isEnglish = value;
+    })
+  }
 }
