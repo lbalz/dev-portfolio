b7276f264bd5ea35364240968a2bf1c73414a7a5
Lukas Balz <lukasbalz.business@gmail.com>
2025-01-15 19:44:36 +0100

    moved footer in main-content, finished footer styles

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { SkillSetComponent } from "./skill-set/skill-set.component";
 import { ReferencesComponent } from "./references/references.component";
 import { ContactMeComponent } from "./contact-me/contact-me.component";
 import { PortfolioProjectsComponent } from "./portfolio-projects/portfolio-projects.component";
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboutMeComponent, SkillSetComponent, ReferencesComponent, ContactMeComponent, PortfolioProjectsComponent],
+  imports: [AboutMeComponent, SkillSetComponent, ReferencesComponent, ContactMeComponent, PortfolioProjectsComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateLanguageService } from '../../translate-language.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  constructor(private languageService: TranslateLanguageService) { }
+
+  logoImgSrc: string = './assets/img/logo_filled.png';
+  email: string = 'lukasbalz.business@gmail.com';
+
+  /*
+     * Translation Service
+     */
+
+  isEnglish: boolean = true;
+
+  translate = inject(TranslateLanguageService);
+
+  ngOnInit(): void {
+    this.languageService.isEnglish$.subscribe(value => {
+      this.isEnglish = value;
+    })
+  }
 }
