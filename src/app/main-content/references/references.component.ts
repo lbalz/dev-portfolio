9e12b2ec8d2b53ebef8a389d6aff6689652dcabc
Lukas Balz <lukasbalz.business@gmail.com>
2025-01-21 19:35:17 +0100

    finished references section

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -11,6 +11,40 @@ import { CommonModule } from '@angular/common';
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
+  currentIndex = 0;
+  references = [
+    {
+      name: 'app.references.team_partner.ben.name',
+      reference: 'app.references.team_partner.ben.reference'
+    },
+    {
+      name: 'app.references.team_partner.can.name',
+      reference: 'app.references.team_partner.can.reference'
+    },
+    {
+      name: 'app.references.team_partner.konstantin.name',
+      reference: 'app.references.team_partner.konstantin.reference'
+    }
+  ];
+
+
+  nextReference() {
+    this.currentIndex = (this.currentIndex + 1) % this.references.length;
+  }
+
+
+  previousReference() {
+    this.currentIndex = (this.currentIndex - 1 + this.references.length) % this.references.length;
+  }
+
+
+  getReferenceClass(index: number): string {
+    const relativeIndex = (index - this.currentIndex + this.references.length) % this.references.length;
+    if (relativeIndex === 0) return 'current';
+    if (relativeIndex === 1) return 'next';
+    if (relativeIndex === this.references.length - 1) return 'previous';
+    return 'hidden';
+  }
 
 
 
